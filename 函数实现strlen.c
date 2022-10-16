cf0d567a24d4e9e22420b9895247897670272bcf
zuiyanw <76681853+zuiyanw@users.noreply.github.com>
2022-10-16 23:30:22 +0800

    Add files via upload

---
+++ 函数实现strlen.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//题目名称：strlen的模拟
+//题目内容：函数实现strlen
+int add(char * str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+		return count;
+}
+int main()
+{
+	char arr[] = {"abcdefg"};
+	int ret =add(arr);//arr为arr[]首元素的地址
+	printf("%d\n", ret);
+	return 0;
+}

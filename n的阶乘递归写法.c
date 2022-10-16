729509cd8e0c01bb204f055762775370ff88e5ba
zuiyanw <76681853+zuiyanw@users.noreply.github.com>
2022-10-16 23:24:08 +0800

    Add files via upload

---
+++ n的阶乘递归写法.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//题目名称：求阶乘
+//题目内容：递归和非递归分别实现n的阶乘
+int fac(int n)
+{
+	if (n > 1)
+	{
+		return n*fac(n - 1);
+	}
+	else
+	{
+		return 1;
+	}
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret=fac(n);
+	printf("%d\n", ret);
+	return 0;
+}
+

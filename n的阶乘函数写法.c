73fd77efcb1e5ed083d6ba7dc915ed9fa36c498b
zuiyanw <76681853+zuiyanw@users.noreply.github.com>
2022-10-16 23:23:48 +0800

    Add files via upload

---
+++ n的阶乘函数写法.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//题目名称：求阶乘
+//题目内容：实现n的阶乘
+void fac(int n)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= n; i++)
+	{
+		ret = i*ret;
+	}
+	printf("%d\n", ret);
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	fac(n);
+	return 0;
+}
